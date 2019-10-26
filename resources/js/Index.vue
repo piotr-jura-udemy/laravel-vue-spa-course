5f1bf3baa3acc36b6a94a87c464348f7540f5601
Piotr Jura <>
2019-10-26 16:22:23 +0200

    #0010-adding-a-navbar

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <router-link :to="{name: 'home'}">Home</router-link>
-    <router-link :to="{name: 'second'}">Second</router-link>
-    <router-view></router-view>
+    <nav class="navbar bg-white border-bottom navbar-light">
+      <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">LaravelBnb</router-link>
+      <router-link class="btn nav-button" :to="{name: 'second'}">Second</router-link>
+    </nav>
+
+    <div class="container mt-4 mb-4 pr-4 pl-4">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
