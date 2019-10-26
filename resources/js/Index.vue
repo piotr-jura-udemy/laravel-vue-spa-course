7c5196869142d773e68f3d072addd5d9d0d65245
Piotr Jura <>
2019-10-26 15:16:40 +0200

    #0009-v-bind-named-route-v-bind-shorthand

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/second">Second</router-link>
+    <router-link :to="{name: 'home'}">Home</router-link>
+    <router-link :to="{name: 'second'}">Second</router-link>
     <router-view></router-view>
   </div>
 </template>
