90351c5ec8d4b8c9684054c62686de3eaf2960ed
Piotr Jura <>
2019-11-01 14:04:59 +0100

    #0014-one-way-data-flow

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -7,6 +7,11 @@
 
 <script>
 export default {
-  props: { itemTitle: String, itemContent: String, price: Number }
+  props: { itemTitle: String, itemContent: String, price: Number },
+  mounted() {
+    console.log(this.itemTitle);
+    // Uncomment to see why modifying prop is a bad idea!
+    // this.itemTitle = "New title";
+  }
 };
 </script>
