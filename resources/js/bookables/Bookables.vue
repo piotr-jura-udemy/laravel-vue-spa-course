4daf52c2343e316b0d165f023e5e6f18dd9e8fe1
Piotr Jura <>
2019-11-01 17:14:38 +0100

    #0016-component-data-state

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <bookable-list-item item-title="Cheap Villa" item-content="A very cheap villa" :price="1000"></bookable-list-item>
     <bookable-list-item
-      item-title="Cheap Villa 2"
-      item-content="A very cheap villa 2"
+      :item-title="bookable1.title"
+      :item-content="bookable1.content"
+      :price="1000"
+    ></bookable-list-item>
+    <bookable-list-item
+      :item-title="bookable2.title"
+      :item-content="bookable2.content"
       :price="1500"
     ></bookable-list-item>
   </div>
@@ -16,11 +20,30 @@ export default {
   components: {
     BookableListItem
   },
+  data() {
+    return {
+      bookable1: {
+        title: "Cheap Villa !!!",
+        content: "A very cheap villa"
+      },
+      bookable2: {
+        title: "Cheap Villa 2",
+        content: "A very cheap villa 2"
+      }
+    };
+  },
   // beforeCreate() {
   //   console.log("before create");
   // },
   created() {
     console.log("created");
+    console.log(this.bookable1);
+    console.log(this.bookable2);
+
+    setTimeout(() => {
+      this.bookable1.title = "Expensive Villa";
+      this.bookable2.title = "Very Expensive Villa";
+    }, 5000);
   }
   // beforeMount() {
   //   console.log("before mount");
