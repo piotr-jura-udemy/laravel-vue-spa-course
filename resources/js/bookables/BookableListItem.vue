81e5de6a3e236475640f4a50ec2a202f3655a347
Piotr Jura <>
2019-11-01 13:09:28 +0100

    #0013-component-props-prop-types

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>Title</h1>
-    <p>Content</p>
+    <h1>{{ itemTitle }}</h1>
+    <p>{{ itemContent }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  props: { itemTitle: String, itemContent: String, price: Number }
+};
+</script>
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
-    <bookable-list-item></bookable-list-item>
+    <bookable-list-item item-title="Cheap Villa" item-content="A very cheap villa" :price="1000"></bookable-list-item>
+    <bookable-list-item
+      item-title="Cheap Villa 2"
+      item-content="A very cheap villa 2"
+      :price="1500"
+    ></bookable-list-item>
   </div>
 </template>
 
