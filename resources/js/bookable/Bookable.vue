f06eacf281e040c6e2dd6370d6988d5fa3aaee6f
Piotr Jura <>
2019-11-07 17:55:30 +0100

    #0033-vue-route-params

---
+++ resources/js/bookable/Bookable.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>xxx</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bookable: null
+    };
+  },
+  created() {
+    axios
+      .get(`/api/bookables/${this.$route.params.id}`)
+      .then(response => (this.bookable = response.data));
+  }
+};
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,6 @@
 import VueRouter from "vue-router";
+import Bookable from "./bookable/Bookable";
 import Bookables from "./bookables/Bookables";
-import Example2 from "./components/Example2";
 
 const routes = [
     {
@@ -9,9 +9,9 @@ const routes = [
         name: "home",
     },
     {
-        path: "/second",
-        component: Example2,
-        name: "second",
+        path: "/bookable/:id",
+        component: Bookable,
+        name: "bookable",
     },
 ];
 
