fb9a844caf0ed72eafe06187c531a19800a0e3dc
Piotr Jura <>
2019-11-09 18:38:52 +0100

    #0041-form-input-binding

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -10,18 +10,36 @@
           name="from"
           class="form-control form-control-sm"
           placeholder="Start date"
+          v-model="from"
         />
       </div>
 
       <div class="form-group col-md-6">
         <label for="to">To</label>
-        <input type="text" name="to" class="form-control form-control-sm" placeholder="End date" />
+        <input
+          type="text"
+          name="to"
+          class="form-control form-control-sm"
+          placeholder="End date"
+          v-model="to"
+        />
       </div>
     </div>
 
     <button class="btn btn-secondary btn-block">Check!</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      from: null,
+      to: null
+    };
+  }
+};
+</script>
 
 <style scoped>
 label {
