fc4dd00c03f93935f132c7f75c08607e79402be3
Piotr Jura <>
2019-11-09 16:37:43 +0100

    #0036-bookable-page-css-grid

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,18 +1,35 @@
 <template>
-  <div>xxx</div>
+  <div class="row">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <div v-if="!loading">
+            <h2>{{ bookable.title }}</h2>
+            <hr />
+            <article>{{ bookable.description }}</article>
+          </div>
+          <div v-else>Loading...</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">availability & prices</div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      bookable: null
+      bookable: null,
+      loading: false
     };
   },
   created() {
-    axios
-      .get(`/api/bookables/${this.$route.params.id}`)
-      .then(response => (this.bookable = response.data));
+    this.loading = true;
+    axios.get(`/api/bookables/${this.$route.params.id}`).then(response => {
+      this.bookable = response.data;
+      this.loading = false;
+    });
   }
 };
 </script>
