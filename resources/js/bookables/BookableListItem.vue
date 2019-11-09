bc393b00b7093807302f3508d19fc16aaf2d2339
Piotr Jura <>
2019-11-09 16:15:28 +0100

    #0035-router-links

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card w-100">
     <div class="card-body">
-      <h5 class="card-title">{{ title }}</h5>
+      <router-link :to="{ name: 'bookable', params: { id } }">
+        <h5 class="card-title">{{ title }}</h5>
+      </router-link>
       <p class="card-text">{{ description }}</p>
     </div>
   </div>
