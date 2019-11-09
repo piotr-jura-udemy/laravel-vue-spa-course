822d2ca570d76f1ac505e4121fb89139d0990c11
Piotr Jura <>
2019-11-09 18:15:31 +0100

    #0040-availability-component

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h6 class="text-uppercase text-secondary font-weight-bolder">Check Availability</h6>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="from">From</label>
+        <input
+          type="text"
+          name="from"
+          class="form-control form-control-sm"
+          placeholder="Start date"
+        />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="to">To</label>
+        <input type="text" name="to" class="form-control form-control-sm" placeholder="End date" />
+      </div>
+    </div>
+
+    <button class="btn btn-secondary btn-block">Check!</button>
+  </div>
+</template>
+
+<style scoped>
+label {
+  font-size: 0.7rem;
+  text-transform: uppercase;
+  color: gray;
+  font-weight: bolder;
+}
+</style>
--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-8 pb-4">
       <div class="card">
         <div class="card-body">
           <div v-if="!loading">
@@ -12,12 +12,19 @@
         </div>
       </div>
     </div>
-    <div class="col-md-4">availability & prices</div>
+    <div class="col-md-4 pb-4">
+      <availability></availability>
+    </div>
   </div>
 </template>
 
 <script>
+import Availability from "./Availability";
+
 export default {
+  components: {
+    Availability
+  },
   data() {
     return {
       bookable: null,
