79ff50c4bcd08b3722d38bd869434ef50d8c6d01
Piotr Jura <>
2019-11-11 13:23:48 +0100

    #0048-local-query-scope

--- app/Booking.php
+++ app/Booking.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
@@ -11,5 +12,11 @@ class Booking extends Model
     public function bookable()
     {
         return $this->belongsTo(Bookable::class);
+    }
+
+    public function scopeBetweenDates(Builder $query, $from, $to)
+    {
+        return $query->where('to', '>=', $from)
+            ->where('from', '<=', $to);
     }
 }
--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Bookable;
 
 class BookableAvailabilityController extends Controller
 {
@@ -13,13 +14,15 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
         $data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after_or_equal:now',
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
 
-        dd($data);
+        $bookable = Bookable::findOrFail($id);
+
+        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
     }
 }
