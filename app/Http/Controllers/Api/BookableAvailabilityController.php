8cc6a858e3be4179f9fdee6f225b4fd405dfd2af
Piotr Jura <>
2019-11-11 12:36:27 +0100

    #0046-single-action-controller

---
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookableAvailabilityController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        dd('Hello I do work');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::get('bookables/{id}', 'Api\BookableController@show');
 
 Route::apiResource('bookables', 'Api\BookableController')->only(['index', 'show']);
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookables.availability.show');
