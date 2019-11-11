7a46bbd5c27b96d1d6cea7026bcd75b8c2d211ab
Piotr Jura <>
2019-11-11 13:04:31 +0100

    #0047-input-validation

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -15,6 +15,11 @@ class BookableAvailabilityController extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd('Hello I do work');
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from'
+        ]);
+
+        dd($data);
     }
 }
