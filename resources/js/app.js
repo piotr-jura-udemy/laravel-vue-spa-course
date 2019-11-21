22c3a5f898093ea4bac968962fea1bdb97fff199
Piotr Jura <>
2019-11-21 17:41:01 +0100

    #0060-moment-and-filters

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 require("./bootstrap");
 
+import moment from "moment";
 import VueRouter from "vue-router";
 import Index from "./Index";
 import router from "./routes";
@@ -7,10 +8,12 @@ import router from "./routes";
 window.Vue = require("vue");
 Vue.use(VueRouter);
 
+Vue.filter("fromNow", value => moment(value).fromNow());
+
 const app = new Vue({
     el: "#app",
     router,
     components: {
-        index: Index,
-    },
+        index: Index
+    }
 });
--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -10,7 +10,7 @@
           <div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
         </div>
         <div class="row">
-          <div class="col-md-12">{{ review.created_at }}</div>
+          <div class="col-md-12">{{ review.created_at | fromNow }}</div>
         </div>
         <div class="row pt-4 pb-4">
           <div class="col-md-12">{{ review.content }}</div>
@@ -21,6 +21,8 @@
 </template>
 
 <script>
+// import moment from "moment";
+
 export default {
   props: {
     bookableId: String
@@ -38,5 +40,10 @@ export default {
       .then(response => (this.reviews = response.data.data))
       .then(() => (this.loading = false));
   }
+  // filters: {
+  //   fromNow(value) {
+  //     return moment(value).fromNow();
+  //   }
+  // }
 };
 </script>
