92057de735dcbef868fd2a2326dcdd1e9f93a006
Piotr Jura <>
2019-11-23 14:02:30 +0100

    #0065-custom-vue-events

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="form-group">
       <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-      <star-rating :rating="5" class="fa-3x"></star-rating>
+      <star-rating :rating="5" class="fa-3x" v-on:rating:changed="review.rating = $event"></star-rating>
     </div>
     <div class="form-group">
       <label for="content" class="text-muted">Describe your expirience with</label>
@@ -12,3 +12,21 @@
     <button class="btn btn-lg btn-primary btn-block">Submit</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      review: {
+        rating: 5,
+        content: null
+      }
+    };
+  }
+  // methods: {
+  //   onRatingChanged(rating) {
+  //     console.log(rating);
+  //   }
+  // }
+};
+</script>
--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="d-flex">
-    <i class="fas fa-star" v-for="star in fullStars" :key="'full' + star"></i>
+    <i
+      class="fas fa-star"
+      v-for="star in fullStars"
+      :key="'full' + star"
+      @click="$emit('rating:changed', star)"
+    ></i>
     <i class="fas fa-star-half-alt" v-if="halfStar"></i>
-    <i class="far fa-star" v-for="star in emptyStars" :key="'empty' + star"></i>
+    <i
+      class="far fa-star"
+      v-for="star in emptyStars"
+      :key="'empty' + star"
+      @click="$emit('rating:changed', fullStars + star)"
+    ></i>
   </div>
 </template>
 
