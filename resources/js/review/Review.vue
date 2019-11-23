c06e3081e9b738a386057ccb4032fea8c099d7bc
Piotr Jura <>
2019-11-23 14:08:58 +0100

    #0066-parent-child-components-communication

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -2,7 +2,11 @@
   <div>
     <div class="form-group">
       <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-      <star-rating :rating="5" class="fa-3x" v-on:rating:changed="review.rating = $event"></star-rating>
+      <star-rating
+        :rating="review.rating"
+        class="fa-3x"
+        v-on:rating:changed="review.rating = $event"
+      ></star-rating>
     </div>
     <div class="form-group">
       <label for="content" class="text-muted">Describe your expirience with</label>
