785608cc7230584235dfb6e5c0d155a3d2ed6f8a
Piotr Jura <>
2019-11-23 13:55:46 +0100

    #0064-review-page

---
+++ resources/js/review/Review.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+      <star-rating :rating="5" class="fa-3x"></star-rating>
+    </div>
+    <div class="form-group">
+      <label for="content" class="text-muted">Describe your expirience with</label>
+      <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+
+    <button class="btn btn-lg btn-primary btn-block">Submit</button>
+  </div>
+</template>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,23 +1,29 @@
 import VueRouter from "vue-router";
 import Bookable from "./bookable/Bookable";
 import Bookables from "./bookables/Bookables";
+import Review from "./review/Review";
 
 const routes = [
     {
         path: "/",
         component: Bookables,
-        name: "home",
+        name: "home"
     },
     {
         path: "/bookable/:id",
         component: Bookable,
-        name: "bookable",
+        name: "bookable"
     },
+    {
+        path: "/review/:id",
+        component: Review,
+        name: "review"
+    }
 ];
 
 const router = new VueRouter({
     routes, // short for `routes: routes`
-    mode: "history",
+    mode: "history"
 });
 
 export default router;
