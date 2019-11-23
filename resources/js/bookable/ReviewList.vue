5612db43224743ed172073403917d7a0d3e03908
Piotr Jura <>
2019-11-23 12:40:33 +0100

    #0063-review-list-cleanup

--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -2,23 +2,13 @@
   <div style="padding: 1.25rem">
     <h6 class="text-uppercase text-secondary font-weight-bolder pt-4">Review List</h6>
 
-    <star-rating :rating="4.0"></star-rating>
-    <star-rating :rating="4.4"></star-rating>
-    <star-rating :rating="4.5"></star-rating>
-    <star-rating :rating="4.6"></star-rating>
-    <star-rating :rating="0.9"></star-rating>
-    <star-rating :rating="3.2"></star-rating>
-    <star-rating :rating="3.5"></star-rating>
-    <star-rating :rating="3.7"></star-rating>
-
     <div v-if="loading">Loading...</div>
     <div v-else>
       <div class="border-bottom d-none d-md-block" v-for="(review, index) in reviews" :key="index">
         <div class="row pt-4">
           <div class="col-md-6">Piotr Jura</div>
           <div class="col-md-6 d-flex justify-content-end">
-            {{ review.rating }}
-            <star-rating :rating="review.rating"></star-rating>
+            <star-rating :rating="review.rating" class="fa-lg"></star-rating>
           </div>
         </div>
         <div class="row">
