5b2243d8712bbcb4782dfc5d2f166d6920d6d5c4
Piotr Jura <>
2019-11-24 11:52:15 +0100

    #0079-fixing-500-on-booking-by-review

--- app/Http/Controllers/Api/BookingByReviewController.php
+++ app/Http/Controllers/Api/BookingByReviewController.php
@@ -17,6 +17,7 @@ class BookingByReviewController extends Controller
      */
     public function __invoke($reviewKey, Request $request)
     {
-        return new BookingByReviewShowResource(Booking::findByReviewKey($reviewKey)) ?? abort(404);
+        $booking = Booking::findByReviewKey($reviewKey);
+        return $booking ? new BookingByReviewShowResource($booking) : abort(404);
     }
 }
