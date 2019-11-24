2cca87ddf90742118b4afc96c2129d3d01914705
Piotr Jura <>
2019-11-24 16:44:48 +0100

    #0092-reading-vuex-state

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -51,8 +51,8 @@ export default {
   },
   data() {
     return {
-      from: null,
-      to: null,
+      from: this.$store.state.lastSearch.from,
+      to: this.$store.state.lastSearch.to,
       loading: false,
       status: null
     };
@@ -62,7 +62,7 @@ export default {
       this.loading = true;
       this.errors = null;
 
-      this.$store.commit('setLastSearch', {
+      this.$store.commit("setLastSearch", {
         from: this.from,
         to: this.to
       });
