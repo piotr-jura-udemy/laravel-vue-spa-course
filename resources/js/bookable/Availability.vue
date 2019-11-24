ab96a1dba63fcd09b0d4c748e66cdb293fe90f79
Piotr Jura <>
2019-11-24 17:29:03 +0100

    #0094-vuex-actions

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -62,7 +62,7 @@ export default {
       this.loading = true;
       this.errors = null;
 
-      this.$store.commit("setLastSearch", {
+      this.$store.dispatch("setLastSearch", {
         from: this.from,
         to: this.to
       });
--- resources/js/store.js
+++ resources/js/store.js
@@ -9,5 +9,11 @@ export default {
         setLastSearch(state, payload) {
             state.lastSearch = payload;
         }
+    },
+    actions: {
+        setLastSearch(context, payload) {
+            context.commit('setLastSearch', payload);
+            localStorage.setItem('lastSearch', JSON.stringify(payload));
+        }
     }
 };
