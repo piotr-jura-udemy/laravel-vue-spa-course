e19e428fc046910e2ab8f564427ae03921088275
Piotr Jura <>
2019-11-25 19:01:24 +0100

    #0100-loading-price-in-bookable

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -23,6 +23,7 @@
 <script>
 import Availability from "./Availability";
 import ReviewList from "./ReviewList";
+import { mapState } from "vuex";
 
 export default {
   components: {
@@ -32,7 +33,8 @@ export default {
   data() {
     return {
       bookable: null,
-      loading: false
+      loading: false,
+      price: null
     };
   },
   created() {
@@ -42,9 +44,23 @@ export default {
       this.loading = false;
     });
   },
+  computed: mapState({
+    lastSearch: "lastSearch"
+  }),
   methods: {
-    checkPrice(hasAvailability) {
-      console.log(hasAvailability);
+    async checkPrice(hasAvailability) {
+      if (!hasAvailability) {
+        this.price = null;
+        return;
+      }
+
+      try {
+        this.price = (await axios.get(
+          `/api/bookables/${this.bookable.id}/price?from=${this.lastSearch.from}&to=${this.lastSearch.to}`
+        )).data.data;
+      } catch (err) {
+        this.price = null;
+      }
     }
   }
 };
