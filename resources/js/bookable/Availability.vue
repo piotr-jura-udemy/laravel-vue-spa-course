cc23292c2a95c66a19572c8af6475b6aab805e9e
Piotr Jura <>
2019-11-25 18:51:55 +0100

    #0099-async-await-availability-emit-event

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -65,7 +65,7 @@ export default {
     };
   },
   methods: {
-    check() {
+    async check() {
       this.loading = true;
       this.errors = null;
 
@@ -74,20 +74,21 @@ export default {
         to: this.to
       });
 
-      axios
-        .get(
+      try {
+        this.status = (await axios.get(
           `/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`
-        )
-        .then(response => {
-          this.status = response.status;
-        })
-        .catch(error => {
-          if (is422(error)) {
-            this.errors = error.response.data.errors;
-          }
-          this.status = error.response.status;
-        })
-        .then(() => (this.loading = false));
+        )).status;
+        this.$emit("availability", this.hasAvailability);
+      } catch (err) {
+        if (is422(err)) {
+          this.errors = err.response.data.errors;
+        }
+
+        this.status = err.response.status;
+        this.$emit("availability", this.hasAvailability);
+      }
+
+      this.loading = false;
     }
   },
   computed: {
--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -15,7 +15,7 @@
       <review-list :bookable-id="this.$route.params.id"></review-list>
     </div>
     <div class="col-md-4 pb-4">
-      <availability :bookable-id="this.$route.params.id"></availability>
+      <availability :bookable-id="this.$route.params.id" @availability="checkPrice($event)"></availability>
     </div>
   </div>
 </template>
@@ -41,6 +41,11 @@ export default {
       this.bookable = response.data.data;
       this.loading = false;
     });
+  },
+  methods: {
+    checkPrice(hasAvailability) {
+      console.log(hasAvailability);
+    }
   }
 };
 </script>
