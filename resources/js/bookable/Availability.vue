c76b03a6de197f17cc95a63b6900c1ae97615637
Piotr Jura <>
2019-11-25 18:25:44 +0100

    #0097-animated-icons

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -38,7 +38,12 @@
       </div>
     </div>
 
-    <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check!</button>
+    <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">
+      <span v-if="!loading">Check!</span>
+      <span v-if="loading">
+        <i class="fas fa-circle-notch fa-spin"></i> Checking...
+      </span>
+    </button>
   </div>
 </template>
 
