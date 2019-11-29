0c7ae444600908ccde417c245c3603f52db36734
Piotr Jura <>
2019-11-29 18:15:16 +0100

    #0109-list-transitions

--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -11,30 +11,32 @@
           </h6>
         </div>
 
-        <div v-for="item in basket" :key="item.bookable.id">
-          <div class="pt-2 pb-2 border-top d-flex justify-content-between">
-            <span>
-              <router-link
-                :to="{name: 'bookable', params: {id: item.bookable.id}}"
-              >{{ item.bookable.title }}</router-link>
-            </span>
-            <span class="font-weight-bold">${{ item.price.total }}</span>
-          </div>
+        <transition-group name="fade">
+          <div v-for="item in basket" :key="item.bookable.id">
+            <div class="pt-2 pb-2 border-top d-flex justify-content-between">
+              <span>
+                <router-link
+                  :to="{name: 'bookable', params: {id: item.bookable.id}}"
+                >{{ item.bookable.title }}</router-link>
+              </span>
+              <span class="font-weight-bold">${{ item.price.total }}</span>
+            </div>
 
-          <div class="pt-2 pb-2 d-flex justify-content-between">
-            <span>From {{ item.dates.from }}</span>
-            <span>To {{ item.dates.to }}</span>
-          </div>
+            <div class="pt-2 pb-2 d-flex justify-content-between">
+              <span>From {{ item.dates.from }}</span>
+              <span>To {{ item.dates.to }}</span>
+            </div>
 
-          <div class="pt-2 pb-2 text-right">
-            <button
-              class="btn btn-sm btn-outline-secondary"
-              @click="$store.dispatch('removeFromBasket', item.bookable.id)"
-            >
-              <i class="fas fa-trash-alt"></i>
-            </button>
+            <div class="pt-2 pb-2 text-right">
+              <button
+                class="btn btn-sm btn-outline-secondary"
+                @click="$store.dispatch('removeFromBasket', item.bookable.id)"
+              >
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            </div>
           </div>
-        </div>
+        </transition-group>
       </div>
     </div>
   </div>
