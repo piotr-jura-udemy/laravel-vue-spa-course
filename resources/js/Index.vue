5f6a50c1b6f5f72e6e79a5ed1e7ce4a4892b190e
Piotr Jura <>
2019-11-29 17:43:36 +0100

    #0107-basket-page

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -3,7 +3,7 @@
     <nav class="navbar bg-white border-bottom navbar-light">
       <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">LaravelBnb</router-link>
 
-      <router-link class="btn nav-button" :to="{name: 'home'}">
+      <router-link class="btn nav-button" :to="{name: 'basket'}">
         Basket
         <span v-if="itemsInBasket" class="badge badge-secondary">{{ itemsInBasket }}</span>
       </router-link>
---
+++ resources/js/basket/Basket.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-8">Checkout form</div>
+      <div class="col-md-4">
+        <div class="d-flex justify-content-between">
+          <h6 class="text-uppercase text-secondary font-weight-bolder">Your Cart</h6>
+          <h6 class="badge badge-secondary text-uppercase">
+            <span v-if="itemsInBasket">Items {{ itemsInBasket }}</span>
+            <span v-else>Empty</span>
+          </h6>
+        </div>
+
+        <div v-for="item in basket" :key="item.bookable.id"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["itemsInBasket"]),
+    ...mapState({
+      basket: state => state.basket.items
+    })
+  }
+};
+</script>
+
+<style scoped>
+h6.badge {
+  font-size: 100%;
+}
+</style>
